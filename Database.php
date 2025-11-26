c568abc8a0e3ec1841d16a6bfb2fccef9a80de15
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-11-26 17:59:36 +0900

    New: Database.php

---
+++ Database.php
@@ -0,0 +1,32 @@
+<?php
+/**	op-unit-database-ci:/Database.php
+ *
+ * @created    2025-11-26
+ * @version    1.0
+ * @package    op-unit-database-ci
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All rights reserved.
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+/* @var $ci \OP\UNIT\CI\CI_Config */
+$ci = OP()->Unit()->CI()->Config();
+
+//	Include sub directory files.
+$name = basename(__FILE__);
+$name = explode('.', $name)[0];
+foreach( glob(__DIR__."/{$name}/*.php") as $path ){
+	require_once($path);
+}
+
+//	...
+return $ci->Get();
