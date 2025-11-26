1878ad1b011304744df24b7a9d7184ac394166cb
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-11-26 18:00:05 +0900

    New: Database > *

---
+++ Database/Transaction.php
@@ -0,0 +1,30 @@
+<?php
+/**	op-unit-database-ci:/Database/Transaction.php
+ *
+ * @created    2025-11-26
+ * @version    1.0
+ * @package    op-unit-database-ci
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All rights reserved.
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+//	...
+$method = basename(__FILE__);
+$method = explode('.', $method)[0];
+
+/* @var $ci \OP\UNIT\CI\CI_Config */
+
+//	...
+$args   =  null;
+$result =  null;
+$ci->Set($method, $result, $args);
