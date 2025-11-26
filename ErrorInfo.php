561b4a92fb6c3ad3ea794c6fe84cedafb89d526e
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-11-26 18:41:31 +0900

    New: ErrorInfo.php

---
+++ ErrorInfo.php
@@ -0,0 +1,31 @@
+<?php
+/**	op-unit-database-ci:/ErrorInfo.php
+ *
+ * @created    2025-11-26
+ * @license    Apache-2.0
+ * @package    op-unit-database-ci
+ * @copyright  (C) 2025 Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+/* @var $ci \OP\UNIT\CI\CI_Config */
+$ci = OP()->Unit()->CI()->Config();
+
+//	Include sub directory files.
+$name = basename(__FILE__);
+$name = explode('.', $name)[0];
+foreach( glob(__DIR__."/{$name}/*.php") as $path ){
+	require_once($path);
+}
+
+//	...
+return $ci->Get();
