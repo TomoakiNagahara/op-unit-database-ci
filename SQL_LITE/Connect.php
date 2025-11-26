03880eafee789a341fc4d1a05a776851c5694442
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-11-26 19:16:14 +0900

    New: SQL_LITE

---
+++ SQL_LITE/Connect.php
@@ -0,0 +1,29 @@
+<?php
+/**	op-unit-database-ci:/SQL_LITE/Connect.php
+ *
+ * @created    2025-11-26
+ * @license    Apache-2.0
+ * @package    op-unit-database-ci
+ * @copyright  (C) 2025 Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+//	...
+$method = basename(__FILE__);
+$method = explode('.', $method)[0];
+
+/* @var $ci \OP\UNIT\CI\CI_Config */
+
+//	...
+$args   = [[]];
+$result = 'Notice: The path is not set.';
+$ci->Set($method, $result, $args);
