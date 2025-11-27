db5a17790ab751d1492fbfef7a5f745b078384ed
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-11-27 13:15:50 +0900

    New: SQL_PG

---
+++ SQL_PG/DSN.php
@@ -0,0 +1,29 @@
+<?php
+/**	op-unit-database-ci:/SQL_PG/DSN.php
+ *
+ * @created    2025-11-26
+ * @license    Apache-2.0
+ * @package    op-unit-database-ci
+ * @copyright  (C) 2025 Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+//	...
+$method = basename(__FILE__);
+$method = explode('.', $method)[0];
+
+/* @var $ci \OP\UNIT\CI\CI_Config */
+
+//	...
+$args   = [[]];
+$result = 'Exception: prod is empty.';
+$ci->Set($method, $result, $args);
