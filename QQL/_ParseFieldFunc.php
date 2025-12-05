c28032aeac28daba7ca63e1e6a36b1557daabed9
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-12-05 14:06:19 +0900

    New: QQL > _ParseFieldFunc.php

--- QQL/_ParseFieldFunc.php
+++ QQL/_ParseFieldFunc.php
@@ -24,6 +24,13 @@ $method = explode('.', $method)[0];
 /* @var $ci \OP\UNIT\CI\CI_Config */
 
 //	...
-$args   = ['',''];
-$result = 'Exception: Call to a member function Quote() on string';
-$ci->Set($method, $result, $args);
+$message = 'Positive case';
+$args   = ['FIELD'];
+$result = '`FIELD`';
+$ci->Set( $method, $result, $args, message:$message );
+
+//	...
+$message = 'Negative case';
+$args   = ['`FIELD'];
+$result = 'Exception: An invalid character string is included: `FIELD';
+$ci->Set( $method, $result, $args, message:$message );
