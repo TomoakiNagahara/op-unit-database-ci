703f012f3826f65db88fd9f942500ba68c8455d9
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-12-05 14:06:03 +0900

    New: QQL > Parse.php

--- QQL/Parse.php
+++ QQL/Parse.php
@@ -24,6 +24,123 @@ $method = explode('.', $method)[0];
 /* @var $ci \OP\UNIT\CI\CI_Config */
 
 //	...
-$args   = ['','',''];
-$result = null;
-$ci->Set($method, $result, $args);
+$message = 'Passed QQL is empty string.';
+$args   = [''];
+$result = [
+	'database' => null,
+	'table'    => '``',
+	'field'    => '*',
+	'where'    => null,
+	'order'    => null,
+	'limit'    => null,
+	'offset'   => null,
+	'group'    => null,
+];
+$ci->Set( $method, $result, $args, message:$message );
+
+//	...
+$message = 'Add table and where.';
+$args   = ['t_testcase.ai = 1'];
+$result = [
+	'database' => null,
+	'table'    => '`t_testcase`',
+	'field'    => '*',
+	'where'    => 'WHERE `ai` = \'1\'',
+	'order'    => null,
+	'limit'    => null,
+	'offset'   => null,
+	'group'    => null,
+];
+$ci->Set( $method, $result, $args, message:$message );
+
+//	...
+$message = 'Add database.';
+$args   = ['testcase.t_testcase.ai = 1'];
+$result = [
+	'database' => '`testcase`',
+	'table'    => '`t_testcase`',
+	'field'    => '*',
+	'where'    => 'WHERE `ai` = \'1\'',
+	'order'    => null,
+	'limit'    => null,
+	'offset'   => null,
+	'group'    => null,
+];
+$ci->Set( $method, $result, $args, message:$message );
+
+//	...
+$message = 'Add limit by string';
+$args   = ['testcase.t_testcase.ai = 1','limit=1'];
+$result = [
+	'database' => '`testcase`',
+	'table'    => '`t_testcase`',
+	'field'    => '*',
+	'where'    => 'WHERE `ai` = \'1\'',
+	'order'    => '',
+	'limit'    => 'LIMIT 1',
+	'offset'   => '',
+	'order'    => '',
+	'group'    => null,
+];
+$ci->Set( $method, $result, $args, message:$message );
+
+//	...
+$message = 'Add offset by string';
+$args   = ['testcase.t_testcase.ai = 1','limit=1, offset=1'];
+$result = [
+	'database' => '`testcase`',
+	'table'    => '`t_testcase`',
+	'field'    => '*',
+	'where'    => 'WHERE `ai` = \'1\'',
+	'order'    => '',
+	'limit'    => 'LIMIT 1',
+	'offset'   => 'OFFSET 1',
+	'order'    => '',
+	'group'    => null,
+];
+$ci->Set( $method, $result, $args, message:$message );
+
+//	...
+$message = 'Add order by string';
+$args   = ['testcase.t_testcase.ai = 1','limit=1, offset=1, order=timestamp'];
+$result = [
+	'database' => '`testcase`',
+	'table'    => '`t_testcase`',
+	'field'    => '*',
+	'where'    => 'WHERE `ai` = \'1\'',
+	'order'    => 'ORDER BY `timestamp`',
+	'limit'    => 'LIMIT 1',
+	'offset'   => 'OFFSET 1',
+	'group'    => null,
+];
+$ci->Set( $method, $result, $args, message:$message );
+
+//	...
+$message = 'Add desc by string';
+$args   = ['testcase.t_testcase.ai = 1','limit=1, offset=1, order=timestamp desc'];
+$result = [
+	'database' => '`testcase`',
+	'table'    => '`t_testcase`',
+	'field'    => '*',
+	'where'    => 'WHERE `ai` = \'1\'',
+	'order'    => 'ORDER BY `timestamp`  desc',
+	'limit'    => 'LIMIT 1',
+	'offset'   => 'OFFSET 1',
+	'group'    => null,
+];
+$ci->Set( $method, $result, $args, message:$message );
+
+//	...
+$message = 'Option is array';
+$args   = ['testcase.t_testcase.ai = 1',['limit'=>'1','offset'=>'1','order'=>'timestamp desc']];
+$result = [
+	'database' => '`testcase`',
+	'table'    => '`t_testcase`',
+	'field'    => '*',
+	'where'    => 'WHERE `ai` = \'1\'',
+	'order'    => 'ORDER BY `timestamp`  desc',
+	'limit'    => 'LIMIT 1',
+	'offset'   => 'OFFSET 1',
+	'group'    => null,
+];
+$ci->Set( $method, $result, $args, message:$message );
