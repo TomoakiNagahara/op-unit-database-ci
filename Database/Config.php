23f2ea747a59a41fbe42d6aee5bd3d7f8eb7cb9f
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-12-05 13:56:35 +0900

    Fix: Database > Config.php

--- Database/Config.php
+++ Database/Config.php
@@ -24,6 +24,6 @@ $method = explode('.', $method)[0];
 /* @var $ci \OP\UNIT\CI\CI_Config */
 
 //	...
-$args   = null;
-$result = [];
+$args   = [];
+$result = null;
 $ci->Set($method, $result, $args);
