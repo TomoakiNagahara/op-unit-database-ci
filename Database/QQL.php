5c8b3aded62c63123500e715cd4253a0cb3bf8a1
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-12-20 15:39:35 +0900

    Fix: Database > QQL.php | php80-sqlite

--- Database/QQL.php
+++ Database/QQL.php
@@ -24,31 +24,40 @@ $method = explode('.', $method)[0];
 /* @var $ci \OP\UNIT\CI\CI_Config */
 
 //	...
+$php = PHP_MAJOR_VERSION.PHP_MINOR_VERSION;
+$ai  = 1;
+$num = 2030;
+if( $php == 80 ){
+	$ai  = (string)$ai;
+	$num = (string)$num;
+}
+
+//	...
 $message = 'Positive case';
 $args    = [' ai <- t_testcase.ai = 1 ', 'limit = 1'];
-$result  = '1';
+$result  = $ai;
 $ci->Set( $method, $result, $args, message:$message );
 
 //	...
 $message = 'Positive case - limit is not set';
 $args    = [' ai <- t_testcase.ai = 1 '];
-$result  = [ 0 => '1' ];
+$result  = [ 0 => $ai ];
 $ci->Set( $method, $result, $args, message:$message );
 
 //	...
 $message = 'Positive case - ai, number';
 $args    = ['ai, number <- t_testcase.ai = 1', 'limit = 1'];
-$result  = ['ai'=>'1', 'number'=>'2030'];
+$result  = ['ai'=>$ai, 'number'=>$num];
 $ci->Set( $method, $result, $args, message:$message );
 
 //	...
 $message = 'Positive case - ai, number, empty option';
 $args    = ['ai, number <- t_testcase.ai = 1'];
-$result  = [0=>['ai'=>'1', 'number'=>'2030']];
+$result  = [0=>['ai'=>$ai, 'number'=>$num]];
 $ci->Set( $method, $result, $args, message:$message );
 
 //	...
 $message = 'Positive case - ai > 0';
 $args    = ['ai, number <- t_testcase.ai > 0', 'limit = 1'];
-$result  = ['ai'=>'1', 'number'=>'2030'];
+$result  = ['ai'=>$ai, 'number'=>$num];
 $ci->Set( $method, $result, $args, message:$message );
